49df36cde8c8457cd9d91e21150cee47159ea020
AlanCienega <craulalan115@gmail.com>
2020-02-13 22:11:03 -0600

    view files

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD en PHP</title>
+    <!--bootstrap4-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!--Fontawesome-->
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">TAREAS</a>
+    </div>
+
+</nav>
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php include("db.php")?>
+<?php include("includes/header.php")?>
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+        <?php if (isset($_SESSION['message'])) { ?>
+            <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
+            <?= $_SESSION['message']?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+        <?php session_unset(); } ?>
+            <div class="card card-body">
+                <form action="saveTask.php" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control" 
+                        name="title" placeholder="nombre de la tarea" autofocus required>
+                    </div>
+                    <div class="form-group">
+                        <textarea name="description"  rows="3" class="form-control" 
+                        placeholder="Descripción" required></textarea>
+                    </div>
+                    <input type="submit" class="btn btn-success btn-block" name="saveTask" value="Guardar">
+                </form>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Descipcion</th>
+                        <th>Creado</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT * FROM tasks";
+                    $tasks = mysqli_query($conn, $query);
+                    while($row = mysqli_fetch_array($tasks)) { ?>
+                        <tr>
+                            <td> <?php echo $row['title'] ?> </td>
+                            <td> <?php echo $row['description'] ?> </td>
+                            <td> <?php echo $row['created_at'] ?> </td>
+                            <td>
+                                <a href="editTask.php?id=<?php echo $row['id'] ?> " 
+                                style="font-size: 2em"> 
+                                    ✏
+                                </a>
+                                <a href="deleteTask.php?id=<?php echo $row['id'] ?> " 
+                                style="font-size: 2em" >
+                                    ✖
+                                </a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include("includes/footer.php")?>
